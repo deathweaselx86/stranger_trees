951e654c338572bb05325c3accaeb4fd6ec7a680
Jessica Ross <deathweasel@gmail.com>
2017-12-14 21:32:25 +0000

    Additional updates to README.md and working program connected to AWS SQS.

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-To install:
+# Stranger Trees
+
+To use:
 
 Prerequirements:
 * Python 2.7+
 * Raspberry Pi 3
 * Linux
+* AWS SQS
+* Fadecandy Controller Board
+* WS281x compatible LED strip(s)
 
+This particular project uses three strips of 50 on 3 channels.
+
+1. You will need to get an OPC server. Get Fadecandy. `cd .. && git clone https://github.com/scanlime/fadecandy.git`
+1. Install the fcserver binary according to the instructions in the server directory of the fadecandy project.
 1. `apt-get install build-essential python-dev git scons swig mesa-common-dev freeglut3-dev`
 1. `pip install virtualenvwrapper` and follow instructions for virtualenvwrapper initial setup
-1. `mkvirtualenv zebratwitter`
+1. `mkvirtualenv strangertrees`
 1. `workon zebratwitter` to use zebratwitter virtualenv
 1. `pip install -Ur requirements.txt`
-1. `git clone https://github.com/jgarff/rpi_ws281x.git`
-1. `cd rpi_ws281x && scons` to compile the rpi\_ws381x library
-1. Carefully follow the instructions to disable BCM2835 sound!
-1. `cd python && sudo python setup.py install` to install the python wrapper
-1. `git clone https://github.com/zestyping/openpixelcontrol.git && cd openpixelcontrol`
-1.
+1. Copy `.env.example` to `.env` and put your Fadecandy server settings and AWS credentials, etc in it.
+It will be picked up by settings.py when you run `main.py`
+1. Run fcserver
+1. `python main.py`
--- main.py
+++ main.py
@@ -1,59 +1,108 @@
 import lib.opc
-import time, copy
+import settings
+import os, time, copy, json
+import boto.sqs
 
-OFF_PIXEL = (0,0,0)
+class StrangerTreeDisplay(object):
+    DEFAULT_MESSAGE = "HAPPY HOLIDAYS"
+    OFF_PIXEL = (0,0,0)
 
-# here is a table matching ASCII characters to
-# pixels in our string and colors
-ASCII_PIXEL_TABLE = {
-    'A': (0, (255,255,255)), # white
-    'B': (1, (255,245,255)),
-    'C': (2, (255,235,255)),
-    'D': (3, (255,225,255)),
-    'E': (4, (255,215,255)),
-    'F': (5, (255,205,255)),
-    'G': (6, (255,195,255)),
-    'H': (7, (255,185,255)),
-    'I': (8, (255,175,255)),
-    'J': (9, (255,165,255)),
-    'K': (10, (255,155,255)),
-    'L': (11, (255,145,255)),
-    'M': (12, (255,135,255)),
-    'N': (13, (255,125,255)),
-    'O': (14, (255,115,255)),
-    'P': (15, (255,105,255)),
-    'Q': (16, (255,95,255)),
-    'R': (17, (255,85,255)),
-    'S': (18, (255,75,255)),
-    'T': (19, (255,65,255)),
-    'U': (20, (255,55,255)),
-    'V': (21, (255,45,255)),
-    'W': (22, (255,35,255)),
-    'X': (23, (255,25,255)),
-    'Y': (24, (255,15,255)),
-    'Z': (25, (255,5,255)),
-    '@': (26, (255,0,255)),
-    '!': (27, (255,0,245)),
-    '.': (28, (255,0,235)),
-    '#': (29, (255,0,225)),
-    '"': (30, (255,0,215)),
-    "'": (30, (255,0,215))
-}
+    # here is a table matching ASCII characters to
+    # pixels in our string, channels, and colors
+    ASCII_PIXEL_TABLE = {
+        'A': (0, (255,255,255), 0), # white
+        'B': (1, (255,245,255), 0),
+        'C': (2, (255,235,255), 0),
+        'D': (3, (255,225,255), 0),
+        'E': (4, (255,215,255), 0),
+        'F': (5, (255,205,255), 0),
+        'G': (6, (255,195,255), 0),
+        'H': (7, (255,185,255), 0),
+        'I': (8, (255,175,255), 0),
+        'J': (9, (255,165,255), 0),
+        'K': (10, (255,155,255), 0),
+        'L': (11, (255,145,255), 0),
+        'M': (12, (255,135,255), 0),
+        'N': (13, (255,125,255), 0),
+        'O': (14, (255,115,255), 0),
+        'P': (15, (255,105,255), 0),
+        'Q': (16, (255,95,255), 0),
+        'R': (17, (255,85,255), 0),
+        'S': (18, (255,75,255), 0),
+        'T': (19, (255,65,255), 0),
+        'U': (20, (255,55,255), 0),
+        'V': (21, (255,45,255), 0),
+        'W': (22, (255,35,255), 0),
+        'X': (23, (255,25,255), 0),
+        'Y': (24, (255,15,255), 0),
+        'Z': (25, (255,5,255), 0),
+        '@': (26, (255,0,255), 0),
+        '!': (27, (255,0,245), 0),
+        '.': (28, (255,0,235), 0),
+        '#': (29, (255,0,225), 0),
+        '"': (30, (255,0,215), 0),
+        "'": (30, (255,0,215), 0)
+    }
 
+    def __init__(self):
+        settings.init()
+        host = os.environ.get('FADECANDY_HOST', 'localhost')
+        port = os.environ.get('FADECANDY_PORT', '7890')
+        self.client = lib.opc.Client('%s:%s' % (host, port))
+     
+        self.region = os.environ.get('AWS_REGION', 'us-east-1')
+        self.access_key = os.environ.get('AWS_ACCESS_KEY', 'XXX')
+        self.secret = os.environ.get('AWS_SECRET_KEY', 'XXX')
+        self.queue_name = os.environ.get('SQS_QUEUE', 'stranger_trees')
+        self.queue = None
 
-def get_reset_pixels():
-    return [copy.deepcopy(OFF_PIXEL) for i in xrange(50)]
+    def start(self):
+        if self.connected() is False:
+            raise StandardError, "Cannot connect to server."
+        self.get_sqs_queue()
+        while True:
+            msg = self.get_sqs_msg()
+            self.display_msg(msg)
+            time.sleep(5)
+
+    def connected(self):
+        self.client.verbose = True
+        return self.client.can_connect()
+
+    def get_sqs_queue(self):
+        connection = boto.sqs.connect_to_region(self.region,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret)
+        self.queue = connection.get_queue(self.queue_name)
+        
+    def get_sqs_msg(self):
+        if self.queue:
+            results = self.queue.get_messages(wait_time_seconds=5)
+            if len(results) > 0:
+                text = json.loads(results[0].get_body())['text']
+                return text.upper()
+            else:
+                return StrangerTreeDisplay.DEFAULT_MESSAGE 
+
+    def display_msg(self, msg):
+        print("Displaying %s" % (msg))
+        for i in range(len(msg)):
+            pixels = self._get_reset_pixels()
+            try:
+                index = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][0]
+                value = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][1]
+                channel = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][2]
+                pixels[index] = value
+                self.client.put_pixels(pixels, channel=channel)
+            except KeyError:
+                self.client.put_pixels(pixels) # this is probably a space; no pixels at all
+            time.sleep(1)
+        pixels = self._get_reset_pixels()
+        self.client.put_pixels(pixels) # defaults to all channels
+
+    def _get_reset_pixels(self):
+        return [copy.deepcopy(StrangerTreeDisplay.OFF_PIXEL) for i in xrange(64)]
 
 if __name__ == '__main__':
-    client = lib.opc.Client('10.0.10.129:7890')
-    test = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ@!.#"'
-    print('testing %(test)s' % {'test': test})
-    for i in range(len(test)):
-        pixels = get_reset_pixels()
-        index = ASCII_PIXEL_TABLE[test[i]][0]
-        value = ASCII_PIXEL_TABLE[test[i]][1]
-        print('lighting up pixel %(index)s for %(value)s' % 
-                {'index': index, 'value': value})
-        pixels[index] = value
-        client.put_pixels(pixels)
-        time.sleep(1)
+    display = StrangerTreeDisplay()
+    display.start()
