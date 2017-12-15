60a425b7e0f3a83413a2768b3d0a5a99568abfa6
Mark Haller <mhaller@thezebra.com>
2017-12-15 16:10:08 -0600

    - Separating SQS setup/config from main class - Adding logger class - Adding argparse with args for: -- queue purge -- add message -- debug

--- README.md
+++ README.md
@@ -16,10 +16,10 @@ This particular project uses three strips of 50 on 3 channels.
 1. Install the fcserver binary according to the instructions in the server directory of the fadecandy project.
 1. `apt-get install build-essential python-dev git scons swig mesa-common-dev freeglut3-dev`
 1. `pip install virtualenvwrapper` and follow instructions for virtualenvwrapper initial setup
-1. `mkvirtualenv strangertrees`
-1. `workon zebratwitter` to use zebratwitter virtualenv
+1. `mkvirtualenv stranger_trees`
+1. `workon stranger_trees` to use zebratwitter virtualenv
 1. `pip install -Ur requirements.txt`
 1. Copy `.env.example` to `.env` and put your Fadecandy server settings and AWS credentials, etc in it.
 It will be picked up by settings.py when you run `main.py`
 1. Run fcserver
-1. `python main.py`
+1. `python main.py --debug`
--- main.py
+++ main.py
@@ -3,10 +3,32 @@ import settings
 import os, time, copy, json
 import boto.sqs
 
-class StrangerTreeDisplay(object):
-    DEFAULT_MESSAGE = "HAPPY HOLIDAYS"
-    OFF_PIXEL = (0,0,0)
+import logging
+import argparse
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+logger = logging.getLogger(__name__)
+
+class QueueWrapper(object):
+
+    def __init__(self, debug=False):
+        self.region = os.environ.get('AWS_REGION', 'us-east-1')
+        self.access_key = os.environ.get('AWS_ACCESS_KEY', 'XXX')
+        self.secret = os.environ.get('AWS_SECRET_KEY', 'XXX')
+        self.queue_name = os.environ.get('SQS_QUEUE', 'stranger_trees')
+
+    def get_queue(self):
+        connection = boto.sqs.connect_to_region(self.region,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret)
+        return connection.get_queue(self.queue_name)
+ 
+
+class StrangerTreeDisplay(object):
+    DEFAULT_MESSAGE = "DEFAULT HOLIDAYS"
+    OFF_PIXEL = (0,0,0)
+    
     # here is a table matching ASCII characters to
     # pixels in our string, channels, and colors
     ASCII_PIXEL_TABLE = {
@@ -44,48 +66,41 @@ class StrangerTreeDisplay(object):
         "'": (30, (255,0,215), 0)
     }
 
-    def __init__(self):
+    def __init__(self, debug=False):
         settings.init()
         host = os.environ.get('FADECANDY_HOST', 'localhost')
         port = os.environ.get('FADECANDY_PORT', '7890')
         self.client = lib.opc.Client('%s:%s' % (host, port))
-     
-        self.region = os.environ.get('AWS_REGION', 'us-east-1')
-        self.access_key = os.environ.get('AWS_ACCESS_KEY', 'XXX')
-        self.secret = os.environ.get('AWS_SECRET_KEY', 'XXX')
-        self.queue_name = os.environ.get('SQS_QUEUE', 'stranger_trees')
+        self.client.verbose = debug
+        self.queue_wrapper = QueueWrapper()
         self.queue = None
 
     def start(self):
         if self.connected() is False:
             raise StandardError, "Cannot connect to server."
-        self.get_sqs_queue()
+        self.queue = self.queue_wrapper.get_queue()
         while True:
             msg = self.get_sqs_msg()
             self.display_msg(msg)
             time.sleep(5)
 
     def connected(self):
-        self.client.verbose = True
         return self.client.can_connect()
 
-    def get_sqs_queue(self):
-        connection = boto.sqs.connect_to_region(self.region,
-                aws_access_key_id=self.access_key,
-                aws_secret_access_key=self.secret)
-        self.queue = connection.get_queue(self.queue_name)
-        
+       
     def get_sqs_msg(self):
         if self.queue:
-            results = self.queue.get_messages(wait_time_seconds=5)
+            results = self.queue.get_messages(wait_time_seconds=5, num_messages=1)
             if len(results) > 0:
+                message = results[0]
                 text = json.loads(results[0].get_body())['text']
+                message.delete()
                 return text.upper()
             else:
                 return StrangerTreeDisplay.DEFAULT_MESSAGE 
 
     def display_msg(self, msg):
-        print("Displaying %s" % (msg))
+        logger.info("Displaying %s" % (msg))
         for i in range(len(msg)):
             pixels = self._get_reset_pixels()
             try:
@@ -103,6 +118,33 @@ class StrangerTreeDisplay(object):
     def _get_reset_pixels(self):
         return [copy.deepcopy(StrangerTreeDisplay.OFF_PIXEL) for i in xrange(64)]
 
+def setup_logger(debug):
+    level = logging.DEBUG if debug else logging.INFO
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+def send_message(message):
+    queue = QueueWrapper().get_queue()
+    queue.write(queue.new_message(body=json.dumps({"text": message})))
+
+def purge_queue(message):
+    queue = QueueWrapper().get_queue()
+    queue.purge()
+
 if __name__ == '__main__':
-    display = StrangerTreeDisplay()
+    parser = argparse.ArgumentParser(description='Light up the stranger_tree based on text messages.')
+    parser.add_argument('--debug', action='store_true', help='do you want verbose debugging messages?')
+    parser.add_argument('--purge', action='store_true', help='clear all messages from queue before processing')
+    parser.add_argument("--message", help="puts a message on the queue before running")
+    args = parser.parse_args()
+    setup_logger(args.debug)
+
+    display = StrangerTreeDisplay(args.debug)
+    if args.purge:
+        purge_queue(args.message)
+    if args.message:
+        send_message(args.message)
     display.start()
