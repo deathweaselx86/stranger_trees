425ba251be1f737a788350834cc60f3a506d8cc3
Jessica Ross <deathweasel@gmail.com>
2017-12-20 23:36:54 +0000

    Success! For our setup, anyway!

--- main.py
+++ main.py
@@ -29,39 +29,36 @@ class StrangerTreeDisplay(object):
     
     # here is a table matching ASCII characters to
     # pixels in our string, channels, and colors
+    # These will all show up on the first string right now
+    # start at 50 and 300
     ASCII_PIXEL_TABLE = {
-        'A': (0, (255,255,255), 0), # white
-        'B': (1, (255,245,255), 0),
-        'C': (2, (255,235,255), 0),
-        'D': (3, (255,225,255), 0),
-        'E': (4, (255,215,255), 0),
-        'F': (5, (255,205,255), 0),
-        'G': (6, (255,195,255), 0),
-        'H': (7, (255,185,255), 0),
-        'I': (8, (255,175,255), 0),
-        'J': (9, (255,165,255), 0),
-        'K': (10, (255,155,255), 0),
-        'L': (11, (255,145,255), 0),
-        'M': (12, (255,135,255), 0),
-        'N': (13, (255,125,255), 0),
-        'O': (14, (255,115,255), 0),
-        'P': (15, (255,105,255), 0),
-        'Q': (16, (255,95,255), 0),
-        'R': (17, (255,85,255), 0),
-        'S': (18, (255,75,255), 0),
-        'T': (19, (255,65,255), 0),
-        'U': (20, (255,55,255), 0),
-        'V': (21, (255,45,255), 0),
-        'W': (22, (255,35,255), 0),
-        'X': (23, (255,25,255), 0),
-        'Y': (24, (255,15,255), 0),
-        'Z': (25, (255,5,255), 0),
-        '@': (26, (255,0,255), 0),
-        '!': (27, (255,0,245), 0),
-        '.': (28, (255,0,235), 0),
-        '#': (29, (255,0,225), 0),
-        '"': (30, (255,0,215), 0),
-        "'": (30, (255,0,215), 0)
+        'A': (333, (255,255,255)), # white
+        'B': (337, (255,245,255)),
+        'C': (324, (255,235,255)),
+        'D': (69, (255,225,255)),
+        'E': (340, (255,215,255)),
+        'F': (318, (255,205,255)),
+        'G': (349, (255,195,255)),
+        'H': (315, (255,185,255)),
+        'I': (85, (255,175,255)),
+        'J': (90, (255,165,255)),
+        'K': (347, (255,155,255)),
+        'L': (52, (255,145,255)),
+        'M': (72, (255,135,255)),
+        'N': (311, (255,125,255)),
+        'O': (83, (255,115,255)),
+        'P': (87, (255,105,255)),
+        'Q': (50, (255,95,255)),
+        'R': (335, (255,85,255)),
+        'S': (320, (255,75,255)),
+        'T': (326, (255,65,255)),
+        'U': (89, (255,55,255)),
+        'V': (304, (255,45,255)),
+        'W': (301, (255,35,255)),
+        'X': (66, (255,25,255)),
+        'Y': (309, (255,15,255)),
+        'Z': (342, (255,5,255)),
+        " ": (94, (255,0,190))
     }
 
     def __init__(self, debug=False):
@@ -104,9 +101,8 @@ class StrangerTreeDisplay(object):
             try:
                 index = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][0]
                 value = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][1]
-                channel = StrangerTreeDisplay.ASCII_PIXEL_TABLE[msg[i]][2]
                 pixels[index] = value
-                self.client.put_pixels(pixels, channel=channel)
+                self.client.put_pixels(pixels)
             except KeyError:
                 self.client.put_pixels(pixels) # this is probably a space; no pixels at all
             time.sleep(1)
@@ -114,7 +110,7 @@ class StrangerTreeDisplay(object):
         self.client.put_pixels(pixels) # defaults to all channels
 
     def _get_reset_pixels(self):
-        return [copy.deepcopy(StrangerTreeDisplay.OFF_PIXEL) for i in xrange(64)]
+        return [copy.deepcopy(StrangerTreeDisplay.OFF_PIXEL) for i in xrange(512)]
 
 def setup_logger(debug):
     level = logging.DEBUG if debug else logging.INFO
